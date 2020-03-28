8628deb0f75a43af10bd1d1901302eb2ac5f303e
kasumikodera <yosaysan1010@gmail.com>
2020-03-28 17:07:55 +0900

    commit

--- js/home.js
+++ js/home.js
@@ -27,6 +27,23 @@ animation: 'kenburns',
  transitionDuration: 4000
    });
 
+$(document).ready(function(){
+if ($(window).width() < 640) {//ウインドウサイズが1024px以下ならば
+	
+	let vh = window.innerHeight * 0.01;
+	// Then we set the value in the --vh custom property to the root of the document
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+	// We listen to the resize event
+	window.addEventListener('resize', () => {
+		// We execute the same script as before
+		let vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+	});
+	
+	}
+});
+
 //replace
 //$('.header_main').height(window.innerHeight + 'px');
 
